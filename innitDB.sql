979869ecfb2a513725f40b4256afdc4b79514bb3
Vagner Ribeiro <vagner@visualsoftware.inf.br>
2024-08-23 16:53:19 -0300

    Add user para testes

--- innitDB.sql
+++ innitDB.sql
@@ -3,6 +3,7 @@ CREATE SCHEMA IF NOT EXISTS bee_database;
 
 -- Uso do Schema
 SET search_path TO bee_database;
+
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -18,3 +19,8 @@ CREATE TABLE messages (
     send_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+INSERT INTO users (id, name, email)
+VALUES
+    ('d290f1ee-6c54-4b01-90e6-d701748f0851', 'Ribeiro', 'ribeiro@example.com'),
+    ('d290f1ee-6c54-4b01-90e6-d701748f0852', 'Vagner', 'vagner@example.com');
